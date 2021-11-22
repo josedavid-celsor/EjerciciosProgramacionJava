e0a9d6eecb4d13ab878da2dc1246f30887f81641
a027356383e <a027356383e@S09-PC03>
2021-11-22 19:49:20 +0100

    Creado proyecto Java

---
+++ EjerciciosProgramacionJava/src/ejerciciosprogramacionjava/EjerciciosProgramacionJava.java
@@ -0,0 +1,24 @@
+/*
+ * Proyecto EjerciciosProgramacionJava - Archivo EjerciciosProgramacionJava.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/license/by-nc-by-sa/4.0/
+ */
+
+package ejerciciosprogramacionjava;
+
+/**
+ *
+ * @author José David Rosales Rios <jdrrdaw@gmail.com>
+ * @version 1.2
+ * @date $[date] $[time]
+ */
+public class EjerciciosProgramacionJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
